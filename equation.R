e740320c19806803b1577f0fac28c7326931ebbf
Meixin Zhang <75701497+Meixin-Zhang@users.noreply.github.com>
2021-01-12 00:31:35 -0800

    Update equation.R

--- equation.R
+++ equation.R
@@ -1,5 +1,6 @@
 Kenya_data_spreadsheet <- read.csv("~/OneDrive - UW/TB-HIV copy/1_8/Kenya_data_spreadsheet.csv")
 lpos <- read.csv("~/OneDrive - UW/TB-HIV copy/1_8/lpos.csv")
+Ipos_Susanne <- read.csv("~/OneDrive - UW/TB-HIV copy/1_8/Ipos_Susanne.csv")
 library(tidyverse)
 
 # calculate the number of HIV infected adults in TB affected households (column N * column HIV_pre)
@@ -14,5 +15,9 @@ HIVpre<-sum(Kenya_data_spreadsheet$N)/sum(Kenya_data_spreadsheet$HIV_positive)
 # The linear relationship between lpos and HIV prevalence
 lm_lpos<-lm(lpos$lpos~lpos$HIV_population_prevalence...)
 summary(lm_lpos) # lpos=43.7406-1.3450*HIVpre
+plot(Ipos_Susanne$HIV_population_prevalence...,Ipos_Susanne$lpos, xlab = "HIV prevalence", ylab = "Ipos")
+lm_ipos<-lm(Ipos_Susanne$lpos~Ipos_Susanne$HIV_population_prevalence...)
+summary(lm_ipos) # Ipos=46.5813 - 1.4836*HIVpre
+abline(lm_ipos)
 
 # incorporate HIVpre into the linear model
